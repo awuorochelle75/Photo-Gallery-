4332b52cdf177cb2f47ebdd0bebf08ddbe63b44b
Rochelle Awuor <awuorochelle75@gmail.com>
2025-04-23 20:10:08 +0300

    changes on src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from "react"
-import Header from "./components/Header.jsx"
-import './App.css'
-import Searchbar from "./components/Searchbar.jsx"
+// App.jsx
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Searchbar from './components/Searchbar';
+import './App.css';
 
 function App() {
   const [title] = useState("PhoGalleria");
 
-
-
   return (
-    <>
+    <Router>
       <Header headtext={title} />
-      <Searchbar />
-    </>
-  )
+      
+      <Routes>
+        {/* Home route - contains Searchbar and future gallery */}
+        <Route path="/" element={
+          <div className="home-page">
+            <Searchbar />
+            {/* Gallery content will go here */}
+          </div>
+        } />
+        
+        {/* Other simple pages */}
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/favorites" element={<div>Favorites</div>} />
+        <Route path="/contact" element={<div>Contact Us</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
