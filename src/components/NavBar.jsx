803b50c64ac24696b8213e427c00f968179a56ff
Rochelle Awuor <awuorochelle75@gmail.com>
2025-04-23 16:01:30 +0300

    modifies files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar ()  {
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#Favorites">Services</a></li>
+        <li><a href="#Favorites">Favorites</a></li>
        
       </ul>
     </nav>
