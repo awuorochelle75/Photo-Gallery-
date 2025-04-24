0647a698d6bfc1521b81c784bc898efa0618657c
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-24 16:28:50 +0300

    adds

--- src/GalleryApp.jsx
+++ src/GalleryApp.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import ImageList from './components/ImageList';
 import axios from 'axios';
-import Favourites from './components/favourites';
 
 const GalleryApp = () => {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  const [favorites, setFavorites] = useState([])
 
 
   const fetchImages = () => {
@@ -33,7 +31,7 @@ const GalleryApp = () => {
   const handleLike = (image) => {
     setFavorites(prev => {
       if (prev.find(img => img.id === image.id)) {
-        return prev.filter(img => img.id !== image.id); 
+        return prev.filter(img => img.id !== image.id);
       } else {
         return [...prev, image];
       }
@@ -63,22 +61,8 @@ const GalleryApp = () => {
     setPage(prev => prev + 1);
   };
 
-  useEffect(() => {
-    const savedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    setFavorites(savedFavorites);
-  }, []); 
-
-  useEffect(() => {
-    localStorage.setItem('favorites', JSON.stringify(favorites));
-  }, [favorites]);
- 
-  const handleRemoveFavorite = (image) => {
-    setFavorites(prev => prev.filter(img => img.id !== image.id));
-  };
-
   return (
     <div className="gallery-container">
-      <Favourites favorites={favorites} onRemove={handleRemoveFavorite}/>
       <h1 className="gallery-title">My Photo Gallery</h1>
       <ImageList images={images}  onLike={handleLike} />
       {isLoading && <p>Loading more images...</p>}
