cd30dc8face8ab36bafe69f4335d24b12782ec22
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-24 09:16:35 +0300

    adds

--- src/GalleryApp.jsx
+++ src/GalleryApp.jsx
@@ -12,7 +12,7 @@ const GalleryApp = () => {
     axios.get(`http://localhost:3000/images`, {
       params: {
         _page: page,
-        _limit: 6  
+        _limit: 6 
       }
     })
     .then((response) => {
