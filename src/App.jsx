e5ae558f10b9ef4a7213df2d38b06f3e85d1196f
Rochelle Awuor <awuorochelle75@gmail.com>
2025-04-24 21:10:36 +0300

    updates files

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-
-// App.jsx
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Searchbar from './components/Searchbar';
 import './App.css';
 import GalleryApp from './GalleryApp';
+import About from './components/About';
+import Contact from './components/Contact';
+import Favourites from './components/Favourites';
 
 function App() {
   const [title] = useState("PhoGalleria");
+  const location = useLocation();
+  
+  // List of paths where header should be hidden
+  const hideHeaderPaths = ['/about', '/contact', '/favorites'];
 
   return (
-    <Router>
-      <Header headtext={title} />
+    <>
+      {/* Show header only if current path is not in hideHeaderPaths */}
+      {!hideHeaderPaths.includes(location.pathname) && <Header headtext={title} />}
       
       <Routes>
-        {/* Home route - contains Searchbar and future gallery */}
         <Route path="/" element={
           <div className="home-page">
-            <Searchbar />
+         
             <GalleryApp />
           </div>
         } />
-        
-        
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/favorites" element={<Favourites />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
-export default App;
