bb991cab6541874a0f5a601450e1598785536569
Nevillemuli <neville.muli@gmail.com>
2025-04-24 08:44:07 +0300

    adds layout for form component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { use, useState} from "react";
 
 function Contact(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault
+    }
+
     return(
-        
+        <div className="contact">
+            <h2>Contact Form</h2>
+            
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="Your Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+                
+                <input
+                type="email"
+                placeholder="Your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                
+                <textarea
+                placeholder="Your Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                rows="4"
+                />
+                
+                <button type="submit">Send</button>
+            </form>
+     </div>       
     )
 } 
 
