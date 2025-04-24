431c5364b4204bd2424d3aec8bba20ee95b67e17
Nevillemuli <neville.muli@gmail.com>
2025-04-24 12:12:34 +0300

    finishes neville branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from "./components/About"
 import GalleryApp from './GalleryApp';
 import Contact from "./components/Contact";
 import './App.css'
-import Favourites from "./components/favourites";
 
 function App() {
 
